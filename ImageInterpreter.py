ba765b863e4d55837477ee7f04db0330b99df6e1
unknown <officialgsproductions@gmail.com>
2016-11-03 22:20:42 -0500

    Split into 2 class files

---
+++ ImageInterpreter.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+from PIL import Image
+from tkFileDialog import askopenfilename
+
+# WARNING: This program can not handle alpha layers.
+# If the image contains an alpha layer,
+# the program will crash with the phrase Too Many Values to Unpack
+class ImageInterpreter:
+
+	allPixels = list()
+
+	#grabs file for use
+	def getFile(self):
+		filename = askopenfilename()
+		print(filename)
+		im = Image.open(filename)
+		print(im.format, im.size, im.mode)
+		# im.show()				#opens file in firefox
+		return im
+
+	#creates matrix of pixel values
+	def createPixelMatrix(self, im):
+		self.allPixels = list(im.getdata())
+		width, height = im.size
+		self.allPixels = [self.allPixels[i * width:(i + 1) * width] for i in xrange(height)]
+
+	#creates output string of ASCII art
+	def createASCIIart(self, im):
+		# create outputString
+		outputString = ""
+		x = 0;
+		y = 0;
+		Xmax, Ymax = im.size
+
+		while (y < Ymax - 1):
+			while (x < Xmax - 1):
+				#black and white value for determining character of
+				summed = sum(self.allPixels[y][x]) / float(len(self.allPixels[y][x]))
+				if summed < 20:
+					outputString += "H"
+				elif summed < 50:
+					outputString += "W"
+				elif summed < 70:
+					outputString += "@"
+				elif summed < 120:
+					outputString += "/"
+				elif summed < 155:
+					outputString += "|"
+				elif summed < 190:
+					outputString += "-"
+				elif summed < 230:
+					outputString += "."
+				else:
+					outputString += " "
+				x += 1
+			x = 0
+			y += 2
+			outputString += "\n"
+		return outputString
+
+	def loopPixels(self, im):
+		self.createPixelMatrix(im)
+		return self.createASCIIart(im)
+
+	def exportTxt(self, outputString):
+		print('exporting string: ')
+		print(str(len(outputString)))
+		# print(outputString)
+		f = open("file.rtf", "w")
+		f.write(outputString)
+		f.close()
--- PyImageToASCII.py
+++ PyImageToASCII.py
@@ -1,74 +1,18 @@
-## -*- coding: UTF-8 -*-
 from __future__ import print_function
 from PIL import Image
 from tkFileDialog import askopenfilename
 import sys
+from ImageInterpreter import ImageInterpreter
 
 
 # WARNING: This program can not handle alpha layers.
 # If the image contains an alpha layer,
 # the program will crash with the phrase Too Many Values to Unpack
 
-def getFile():  # asks for an image file and returns it
-    filename = askopenfilename()
-    print(filename)
-    im = Image.open(filename)
-    print(im.format, im.size, im.mode)
-    # im.show()                                     #opens file in firefox
-    return im
-
-
-def loopPixels(im):
-    outputString = ""
-    allPixels = list(im.getdata())
-    width, height = im.size
-
-    allPixels = [allPixels[i * width:(i + 1) * width] for i in xrange(height)]
-
-    # create outputString
-    x = 0;
-    y = 0;
-    Xmax, Ymax = im.size
-
-    while (y < Ymax-1):
-        while (x < Xmax-1):
-            summed = sum(allPixels[y][x]) / float(len(allPixels[y][x]))
-            if summed < 20:
-                outputString += "H"
-            elif summed < 50:
-                outputString += "W"
-            elif summed < 70:
-                outputString += "@"
-            elif summed < 120:
-                outputString += "/"
-            elif summed < 155:
-                outputString += "|"
-            elif summed < 190:
-                outputString += "-"
-            elif summed < 230:
-                outputString += "."
-            else:
-                outputString += " "
-            x += 1
-        x = 0
-        y += 2
-        outputString += "\n"
-
-    return outputString
-
-
-def exportTxt(outputString):
-    print('exporting string: ')
-    print(str(len(outputString)))
-    #print(outputString)
-    f = open("file.rtf", "w")
-    f.write(outputString)
-    f.close()
-
-
 #####
 ##MAIN FUNCTION
 #####
-im = getFile()
-outputString = loopPixels(im)
-exportTxt(outputString)
+imInterpreter = ImageInterpreter()				#create image processor
+im = imInterpreter.getFile()					#get file to use with image interpreter
+outputString = imInterpreter.loopPixels(im)		#Interpret file to black and white and create string
+imInterpreter.exportTxt(outputString)           #output string to rtf file
